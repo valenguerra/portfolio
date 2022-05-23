55656f3ada941736b4612ea567071a73d372b9b8
Valentino Guerra <valenwerra@gmail.com>
2022-05-23 16:12:33 -0300

    remove react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
 
 const rootElement = document.getElementById("root");
@@ -10,11 +9,6 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-    ,
+    <Home />
   </React.StrictMode>
 );
