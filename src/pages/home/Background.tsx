6ad6e417d92891bdc97d0fb1bbfa5af0262a3152
Valentino Guerra <valenwerra@gmail.com>
2022-05-30 20:18:53 -0300

    fixed background on responsive

--- src/pages/home/Background.tsx
+++ src/pages/home/Background.tsx
@@ -2,15 +2,19 @@ import bg01 from "../../assets/images/bg01.png";
 import bg02 from "../../assets/images/bg02.png";
 import bg03 from "../../assets/images/bg03.png";
 import bg04 from "../../assets/images/bg04.png";
-
+import { ImageProps } from "../../types";
 
 export const Background = (): JSX.Element => {
   return (
-    <div className='absolute inset-0 -z-10'>
-      <img src={bg01} alt="Mountains" className="min-h-screen object-cover"/>
-      <img src={bg02} alt="Cave" className="min-h-screen object-cover"/>
-      <img src={bg03} alt="Submarine cave" className="min-h-screen object-cover"/>
-      <img src={bg04} alt="Deep ocean" className="min-h-screen object-cover"/>
+    <div className="absolute top-0 bottom-0 -z-10 flex flex-col justify-between xl:bottom-auto xl:justify-start">
+      <Image src={bg01} alt="Mountains" />
+      <Image src={bg02} alt="Cave" />
+      <Image src={bg03} alt="Submarine cave" />
+      <Image src={bg04} alt="Deep ocean" />
     </div>
   );
+};
+
+const Image = ({ src, alt }: ImageProps) => {
+  return <img src={src} alt={alt} className="min-h-screen object-cover" />;
 };
